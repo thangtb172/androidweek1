f2020adf3d42a12d583fe604025ef64678b7008f
Thangtb <thangtb.2113@gmail.com>
2021-04-14 22:54:23 +0700

    Week3

--- app/src/main/java/com/example/week1/MainActivity.kt
+++ app/src/main/java/com/example/week1/MainActivity.kt
@@ -12,25 +12,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        val edt1 = findViewById<EditText>(R.id.edt1);
-        val edt2 = findViewById<EditText>(R.id.edt2);
-        val btn = findViewById<Button>(R.id.btn);
-        btn.setOnClickListener {
-            val username = "ronaldo@gmail.com";
-            val password = "123456";
-            if(edt1.text.toString() == username && edt2.text.toString() == password ){
-                Toast.makeText(applicationContext,"Account Hợp Lệ",Toast.LENGTH_LONG).show();
-                val intent = Intent(this@MainActivity,ProfileActivity::class.java)
-                startActivity(intent)
-
-            }
-            else {
-                Toast.makeText(applicationContext,"Mời nhập lại ",Toast.LENGTH_LONG).show();
-            }
-
-
-        }
+        setContentView(R.layout.activity_signup)
+//        val edt1 = findViewById<EditText>(R.id.edt1);
+//        val edt2 = findViewById<EditText>(R.id.edt2);
+//        val btn = findViewById<Button>(R.id.btn);
+//        btn.setOnClickListener {
+//            val username = "ronaldo@gmail.com";
+//            val password = "123456";
+//            if(edt1.text.toString() == username && edt2.text.toString() == password ){
+//                Toast.makeText(applicationContext,"Account Hợp Lệ",Toast.LENGTH_LONG).show();
+//                val intent = Intent(this@MainActivity,ProfileActivity::class.java)
+//                startActivity(intent)
+//
+//            }
+//            else {
+//                Toast.makeText(applicationContext,"Mời nhập lại ",Toast.LENGTH_LONG).show();
+//            }
+//
+//
+//        }
     }
 
 }
